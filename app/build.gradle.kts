44859a3cf6c0221fc2b709d843b90d4d86a50bc7
Mike Wielgosz <598094+mwielgosz@users.noreply.github.com>
2025-03-05 13:20:09 -0500

    libs.versions.toml: change androidx-navigation-compose-v276 name to not include 'v276'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation (libs.androidx.material.icons.extended)
-    implementation(libs.androidx.navigation.compose.v276)
+    implementation(libs.androidx.navigation.compose)
     // Retrofit with Kotlinx Converter & OkHttp
     implementation(libs.retrofit)
     implementation(libs.converter.kotlinx.serialization)
